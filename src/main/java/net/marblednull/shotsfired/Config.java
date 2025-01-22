8a493f37656e9eb8edcaadf8df0aeafe485aae14
MelodicCougar7 <melodiccougar7@gmailc.om>
2025-01-22 16:01:23 -0500

    More updated meta data

--- src/main/java/net/marblednull/shotsfired/Config.java
+++ src/main/java/net/marblednull/shotsfired/Config.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Config {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    public static ForgeConfigSpec.ConfigValue<List<String>> CASING_TO_GUNID = BUILDER.define("gunCasingMatch", List.of("tacz:glock_17|massmunitions:556x45_casing", "tacz:cz75|massmunitions:50bmg_casing"));
+    public static ForgeConfigSpec.ConfigValue<List<String>> CASING_TO_GUNID = BUILDER.define("gunCasingMatch", List.of("tacz:glock_17|minecraft:apple", "tacz:cz75|minecraft:apple"));
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 
