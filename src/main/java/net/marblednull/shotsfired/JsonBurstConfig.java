0625f2a09b26aba1ac9e4ab0f24f57db8eea7f25
MelodicCougar7 <melodiccougar7@gmailc.om>
2025-01-22 16:56:23 -0500

    Testing new burst fire config

---
+++ src/main/java/net/marblednull/shotsfired/JsonBurstConfig.java
@@ -0,0 +1,64 @@
+package net.marblednull.shotsfired;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+
+/// Because we couldn't get the Forge Config to work for the life of us
+/// Tweaked by MC7 and ChatGPT for burst fire.
+public class JsonBurstConfig {
+    public static final Path DIR = FMLPaths.CONFIGDIR.get();
+    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    public static HashMap<String, Integer> CONFIG_MAP = new HashMap<>();
+
+    public static HashMap<String, Integer> readConfig() throws IOException {
+        File file = DIR.resolve("shotsfiredburst.json").toFile();
+        if(file.exists()) {
+            FileReader reader = new FileReader(file);
+            List<String> stringList = GSON.fromJson(reader, List.class);
+            HashMap<String, Integer> map = new HashMap<>();
+
+            for (String strToParse : stringList) {
+                String gunId;
+               Integer value;
+                String[] strs = strToParse.split("\\|");
+                gunId = strs[0];
+                try {
+                    value = Integer.parseInt(strs[1]); // Parse the integer value
+                } catch (NumberFormatException e) {
+                    value = 0; // Default value in case of parsing error
+                }
+                map.put(gunId, value);
+            }
+
+            return map;
+        } else {
+            checkConfig();
+            return new HashMap<String, Integer>();
+        }
+    }
+
+    public static void checkConfig() throws IOException {
+        File file = DIR.resolve("shotsfiredburst.json").toFile();
+        if(!file.exists()) {
+            FileWriter writer = new FileWriter(file);
+            JsonArray strArr = new JsonArray();
+            strArr.add("tacz:hk_mp5a5|3");
+            strArr.add("tacz:scar_l|3");
+            System.out.println("JSON CONFIG GEN = " + GSON.toJson(strArr));
+            writer.write(GSON.toJson(strArr));
+            writer.close();
+        }
+        CONFIG_MAP = readConfig();
+    }
+}
--- src/main/java/net/marblednull/shotsfired/ModEvents.java
+++ src/main/java/net/marblednull/shotsfired/ModEvents.java
@@ -1,8 +1,10 @@
 package net.marblednull.shotsfired;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -38,13 +40,17 @@ public class ModEvents {
                 ItemStack casingStack = new ItemStack(gunItem);
 
                 if (gunEvent.getShooter().getMainHandItem().getTag().getString("GunFireMode").equals("BURST")) {
-                    //burst fire mode spawning two casings
-                    for (int i = 0; i < 2; i++) {
-                        //Create casing entity
-                        ItemEntity casing = new ItemEntity(gunEvent.getShooter().level(), gunEvent.getShooter().getX(), gunEvent.getShooter().getY(), gunEvent.getShooter().getZ(), casingStack.copy());
-                        casing.setPickUpDelay(20);
-                        //Add casing
-                        gunEvent.getShooter().level().addFreshEntity(casing);
+                    HashMap<String, Integer> gunBurstMap = JsonBurstConfig.CONFIG_MAP;
+                    if (gunBurstMap.containsKey(gunId)) {
+                        int burstCount = gunBurstMap.getOrDefault(gunId, 1);
+                        //burst fire mode spawning two casings
+                        for (int i = 0; i < burstCount; i++) {
+                            //Create casing entity
+                            ItemEntity casing = new ItemEntity(gunEvent.getShooter().level(), gunEvent.getShooter().getX(), gunEvent.getShooter().getY(), gunEvent.getShooter().getZ(), casingStack.copy());
+                            casing.setPickUpDelay(20);
+                            //Add casing
+                            gunEvent.getShooter().level().addFreshEntity(casing);
+                        }
                     }
                 } else {
                     //Create casing entity
